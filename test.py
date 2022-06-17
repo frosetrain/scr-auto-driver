7a21feb80dc02ac99eda054ee415c1a1f1f406fa
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2022-06-17 12:37:03 +0800

    Update test.py
    Signed-off-by: Frosetrain <80436189+frosetrain@users.noreply.github.com>

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ keyboard = pynput.keyboard.Controller()
 mouse = pynput.mouse.Controller()
 
 
-<<<<<<< HEAD
 def change_throttle(percent):
     if percent > 0:
         keyboard.press('w')
@@ -20,18 +19,6 @@ def change_throttle(percent):
         keyboard.press('s')
         threading.Event().wait(0.03362 * abs(percent))
         keyboard.release('s')
-=======
-def increase_throttle(percent):
-    keyboard.press('w')
-    threading.Event().wait(0.033624 * percent)
-    keyboard.release('w')
-
-
-def decrease_throttle(percent):
-    keyboard.press('s')
-    threading.Event().wait(0.033624 * percent)
-    keyboard.release('s')
->>>>>>> 126e4bc176ede68ec7e3d5d55ae45811f005ba5c
 
 
 if __name__ == "__main__":
