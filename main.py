4f361005d7ccad318f980c4189ef0659f0fb4db7
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2023-03-11 16:37:21 +0800

    Kind of works now
    Except for stopping at stations (how to do stopping?)

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
-#!/usr/bin/env python3
-
 import logging
-import threading
+from os import system
+from threading import Event
 from time import sleep
 
 import pynput.keyboard
-import pynput.mouse
 import pytesseract
 from PIL import ImageGrab
 
+window = (0, 100, 1920, 1180)
+speed_limit_bbox = (window[0] + 865, window[1] + 940, window[0] + 925, window[1] + 980)
+signal_bbox = (1685, window[1] + 893, 1686, window[1] + 1040)
+signal_points = ((0, 0), (0, 49), (0, 98), (0, 146))
+aws_bbox = (1430, 1060, 1431, 1061)
+top_speed = 100
+throttle_change_speed = 0.033624
+
 keyboard = pynput.keyboard.Controller()
-# mouse = pynput.mouse.Controller()
 
 logging.getLogger("PIL.TiffImagePlugin").setLevel(logging.INFO)
 logging.basicConfig(level="DEBUG")
 
 
-TOP_SPEED = 100
-THROTTLE_CHANGE_SPEED = 0.033624
-SPEED_LIMIT_BBOX = (1561, 1352, 1594, 1373)
-SIGNAL_LIGHT_BBOX = (2090, 1302, 2118, 1425)
-SIGNAL_LIGHT_POINTS = ((14, 14), (14, 46), (14, 78), [14, 110])
-AWS_BBOX = (2025, 1320, 2035, 1330)
-AWS_POINT = (2030, 1325)
-
-
 def get_speed_limit() -> int:
-    pic = ImageGrab.grab(bbox=SPEED_LIMIT_BBOX)
-    pic = pic.resize((99, 63))
-    pic.save("speed_limit_snip.tif")
+    pic = ImageGrab.grab(bbox=speed_limit_bbox)
+    pic = pic.resize((90, 60))
+    pic.save("speed_limit.tif")
     speed_limit = pytesseract.image_to_string(
-        "speed_limit_snip.tif", config="--psm 7 digits get.images"
+        "speed_limit.tif", config="--psm 7 digits get.images"
     )
     speed_limit = speed_limit.strip()
-    logging.debug(f"Tesseract returned {speed_limit}")
     try:
         speed_limit = int(speed_limit)
-        if speed_limit <= 10:
-            speed_limit = 75
-        logging.debug(f"Succesfully read speed limit: {speed_limit}")
-        last_successful_speed_limit = speed_limit
+        logging.debug(f"Speed limit: {speed_limit}")
     except ValueError:
-        try:
-            speed_limit = last_successful_speed_limit
-            logging.debug(f"Tesseract error, setting speed to {speed_limit}")
-        except UnboundLocalError:
-            return
+        logging.debug(f"Tesseract error: {speed_limit}")
     return speed_limit
 
 
 def get_signal_limit() -> int:
-    pic = ImageGrab.grab(bbox=SIGNAL_LIGHT_BBOX)
+    pic = ImageGrab.grab(bbox=signal_bbox)
+    pic.save("signal.tif")
     pixels = pic.load()
 
     rgb_values = []
-    for point in SIGNAL_LIGHT_POINTS:
+    for point in signal_points:
         rgb_values.append(pixels[point[0], point[1]])
 
     aspect_colours = []
@@ -72,9 +61,9 @@ def get_signal_limit() -> int:
             aspect_colours.append("b")
 
     if aspect_colours == ["b", "g", "b", "b"]:
-        signal_limit = TOP_SPEED
+        signal_limit = top_speed
     elif aspect_colours == ["y", "b", "y", "b"]:
-        signal_limit = TOP_SPEED
+        signal_limit = top_speed
     elif aspect_colours == ["b", "b", "y", "b"]:
         signal_limit = 45
     elif "r" in aspect_colours:
@@ -82,38 +71,41 @@ def get_signal_limit() -> int:
     else:
         signal_limit = 100
 
+    logging.debug(f"Signal limit: {signal_limit}")
     return signal_limit
-
-
-def handle_aws() -> None:
-    pic = ImageGrab.grab(bbox=AWS_BBOX)
-    pixels = pic.load()
-    if pixels[AWS_POINT][0] > 240:
-        keyboard.press("q")
 
 
 def change_throttle(percent):
     if percent > 0:
         keyboard.press("w")
-        threading.Event().wait(THROTTLE_CHANGE_SPEED * percent)
+        Event().wait(throttle_change_speed * percent)
         keyboard.release("w")
     elif percent < 0:
         keyboard.press("s")
-        threading.Event().wait(THROTTLE_CHANGE_SPEED * abs(percent))
+        Event().wait(throttle_change_speed * abs(percent))
         keyboard.release("s")
+
+
+def handle_aws() -> None:
+    pic = ImageGrab.grab(bbox=aws_bbox)
+    pixels = pic.load()
+    if pixels[0, 0][0] > 240:
+        keyboard.press("q")
+        logging.debug("AWS")
 
 
 if __name__ == "__main__":
     current_target_speed = 0
     sleep(2)
     while True:
+        system("clear")
         got_speed_limit = get_speed_limit()
         got_signal_limit = get_signal_limit()
-        if got_speed_limit is not None:
+        if isinstance(got_speed_limit, int):
             new_target_speed = min(got_speed_limit, got_signal_limit)
         else:
             new_target_speed = got_signal_limit
-        logging.debug(f"Target speed {new_target_speed}")
+        logging.debug(f"Target speed: {new_target_speed}")
         if new_target_speed != current_target_speed:
             if new_target_speed > 0:
                 throttle_change_amount = new_target_speed - current_target_speed
@@ -123,4 +115,4 @@ if __name__ == "__main__":
             change_throttle(throttle_change_amount)
             current_target_speed = new_target_speed
         handle_aws()
-        sleep(2)
+        sleep(0.5)
