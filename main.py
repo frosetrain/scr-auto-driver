e243b3ebe69c2195e1f94b2ca8b7837ea859bbb9
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2023-03-13 20:10:45 +0800

    Add station distance and fix AWS

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from os import system
 from threading import Event
 from time import sleep
 
-import pynput.keyboard
 import pytesseract
 from PIL import ImageGrab
+from pynput.keyboard import Controller
 
 window = (0, 100, 1920, 1180)
 speed_limit_bbox = (window[0] + 865, window[1] + 940, window[0] + 925, window[1] + 980)
-signal_bbox = (1685, window[1] + 893, 1686, window[1] + 1040)
+signal_bbox = (window[0] + 1685, window[1] + 893, window[0] + 1686, window[1] + 1040)
 signal_points = ((0, 0), (0, 49), (0, 98), (0, 146))
-aws_bbox = (1430, 1060, 1431, 1061)
+aws_bbox = (window[0] + 1430, window[1] + 960, window[0] + 1431, window[1] + 961)
+distance_bbox = (window[0] + 350, window[1] + 1023, window[0] + 426, window[1] + 1068)
 top_speed = 100
-throttle_change_speed = 0.033624
+throttle_change_speed = 0.0337
 
-keyboard = pynput.keyboard.Controller()
+keyboard = Controller()
 
 logging.getLogger("PIL.TiffImagePlugin").setLevel(logging.INFO)
 logging.basicConfig(level="DEBUG")
@@ -31,7 +32,6 @@ def get_speed_limit() -> int:
     speed_limit = speed_limit.strip()
     try:
         speed_limit = int(speed_limit)
-        logging.debug(f"Speed limit: {speed_limit}")
     except ValueError:
         logging.debug(f"Tesseract error: {speed_limit}")
     return speed_limit
@@ -39,7 +39,6 @@ def get_speed_limit() -> int:
 
 def get_signal_limit() -> int:
     pic = ImageGrab.grab(bbox=signal_bbox)
-    pic.save("signal.tif")
     pixels = pic.load()
 
     rgb_values = []
@@ -71,48 +70,126 @@ def get_signal_limit() -> int:
     else:
         signal_limit = 100
 
-    logging.debug(f"Signal limit: {signal_limit}")
     return signal_limit
 
 
-def change_throttle(percent):
-    if percent > 0:
-        keyboard.press("w")
-        Event().wait(throttle_change_speed * percent)
-        keyboard.release("w")
-    elif percent < 0:
-        keyboard.press("s")
-        Event().wait(throttle_change_speed * abs(percent))
-        keyboard.release("s")
+def get_station_distance() -> float:
+    pic = ImageGrab.grab(bbox=distance_bbox)
+    pic.save("distance.tif")
+    distance = pytesseract.image_to_string(
+        "distance.tif", config="--psm 7 digits get.images"
+    )
+    distance = distance.strip()
+    try:
+        distance = float(distance)
+    except ValueError:
+        distance = 1.0
+        logging.debug(f"Tesseract error: {distance}")
+    return distance
 
 
 def handle_aws() -> None:
     pic = ImageGrab.grab(bbox=aws_bbox)
     pixels = pic.load()
-    if pixels[0, 0][0] > 240:
+    if pixels[0, 0][0] > 200:
         keyboard.press("q")
         logging.debug("AWS")
+
+
+def change_throttle(start, end):
+    if end == 0 or end == 100:
+        buffer = 0.1
+    else:
+        buffer = 0
+    if end > start:
+        keyboard.press("w")
+        Event().wait(throttle_change_speed * (end - start) + buffer)
+        keyboard.release("w")
+    elif end < start:
+        keyboard.press("s")
+        Event().wait(throttle_change_speed * (start - end) + buffer)
+        keyboard.release("s")
+
+
+class Station:
+    def __init__(self, name, entry_speed, time1, time2, time3):
+        self.name = name
+        self.entry_speed = entry_speed
+        self.time1 = time1  # entry speed to 20
+        self.time2 = time2  # 20 to 5
+        self.time3 = time3  # 5 to 0
+
+
+nb_stations = [
+    Station("Stepford East", 45, 0, 0, 0),
+    Station("Stepford High Street", 45, 0, 0, 0),
+    Station("Whitefield Lido", 45, 0, 0, 0),
+    Station("Stepford United Football Club", 45, 0, 0, 0),
+]
+sb_stations = [
+    Station("Stepford United Football Club", 45, 0, 0, 0),
+    Station("Whitefield Lido", 45, 0, 0, 0),
+    Station("Stepford High Street", 45, 0, 0, 0),
+    Station("Stepford East", 45, 0, 0, 0),
+    Station("Stepford Central", 45, 0, 0, 0),
+]
+station_names = [station.name for station in stations]
+
+
+def station_stop(current_target_speed):
+    # Get station name
+    station_name = "Whitefield Lido"  # TODO: Use Tesseract to obtain this
+    station = stations[station_names.index(station_name)]
+    reached = False
+    while not reached:
+        distance = get_station_distance()
+        if distance == 0.0:
+            reached = True
+    sleep(station.time1)
+    change_throttle(current_target_speed, 20)
+    sleep(station.time2)
+    change_throttle(20, 5)
+    sleep(station.time3)
+    change_throttle(5, 0)
 
 
 if __name__ == "__main__":
     current_target_speed = 0
     sleep(2)
     while True:
-        system("clear")
-        got_speed_limit = get_speed_limit()
-        got_signal_limit = get_signal_limit()
-        if isinstance(got_speed_limit, int):
-            new_target_speed = min(got_speed_limit, got_signal_limit)
+        # Read values from screen
+        speed_limit = get_speed_limit()
+        signal_limit = get_signal_limit()
+        station_distance = get_station_distance()
+        if station_distance < 0.14:
+            station_limit = 45
+        if station_distance < 0.03:
+            station_stop()
+            # This function skips the speed limit and signal,
+            # and makes the train stop at the station correctly.
+            # The main loop continues once the stop is complete.
+            continue
         else:
-            new_target_speed = got_signal_limit
-        logging.debug(f"Target speed: {new_target_speed}")
+            station_limit = top_speed
+
+        if isinstance(speed_limit, int):
+            new_target_speed = min(speed_limit, signal_limit, station_limit)
+        else:
+            new_target_speed = signal_limit
+
+        # Change the throttle
         if new_target_speed != current_target_speed:
-            if new_target_speed > 0:
-                throttle_change_amount = new_target_speed - current_target_speed
-            elif new_target_speed == 0:
-                throttle_change_amount = new_target_speed - current_target_speed - 5
             logging.info(f"Changing speed to {new_target_speed}")
-            change_throttle(throttle_change_amount)
+            change_throttle(current_target_speed, new_target_speed)
             current_target_speed = new_target_speed
+
         handle_aws()
-        sleep(0.5)
+
+        system("clear")
+        logging.debug(f"Speed limit: {speed_limit}")
+        logging.debug(f"Signal limit: {signal_limit}")
+        logging.debug(f"Distance to station: {station_distance}")
+        logging.debug(f"Station limit: {station_limit}")
+        logging.debug(f"Target speed: {new_target_speed}")
+
+        # sleep(0.5)
