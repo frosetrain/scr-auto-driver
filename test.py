33678c91ffbff2c657aa2e614402df7f3cf8d02f
frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-11-16 16:12:21 +0800

    Add buffer from 0 to 5 speed

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ throttle_change_speed = 0.04035789
 
 
 def change_throttle(start, end) -> None:
+    if start == 0:
+        buffer = -1/60
+        start = 5
     if end == 0 or end == top_speed:
         buffer = 0.1
     else:
@@ -19,3 +22,13 @@ def change_throttle(start, end) -> None:
         keyboard.press("s")
         sleep(100 / top_speed * throttle_change_speed * (start - end) + buffer)
         keyboard.release("s")
+
+
+sleep(2)
+# change_throttle(0, 99)
+# sleep(1)
+# change_throttle(99, 50)
+# sleep(1)
+# change_throttle(50, 0)
+# sleep(1)
+change_throttle(0, 80)
